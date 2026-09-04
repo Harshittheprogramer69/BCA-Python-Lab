b15d66d5460139329fd23a3425f70840f10d939b
Harshit_khanna <117803004+Harshittheprogramer69@users.noreply.github.com.>
2026-09-04 12:43:07 +0530

    Added Program 11 - Display Pyramid

---
+++ Programs/11.Display_Pyramid.py
@@ -0,0 +1,9 @@
+def Pyramid(n):
+    for i in range(1,n+1):
+        for j in range(1,i+1):
+            print("*", end= ' ')
+        print("\n")
+
+number = int(input("Enter a number: "))
+
+Pyramid(number)
